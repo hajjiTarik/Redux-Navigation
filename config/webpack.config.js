959899b0ed9e8325a1458a7dab75556ca7af391b
Hajji Tarik <hajji-tarik@outlook.fr>
2017-05-23 12:18:44 +0000

    Feature/nav (#13)
    * fix eslint
    
    * fix error and add yarn
    
    * webpack configuration

---
+++ config/webpack.config.js
@@ -0,0 +1,50 @@
+var webpack = require('webpack');
+var env = process.env.NODE_ENV;
+var path = require('path');
+
+const rootFolder = path.join(__dirname, '../');
+module.exports = {
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'stage-0'],
+        },
+      },
+    ],
+  },
+  entry: path.join(__dirname, '../example/src/index.js'),
+  output: {
+    filename: 'bundle.js',
+    library: 'redux-navigation',
+    libraryTarget: 'umd',
+    path: path.resolve(path.join(rootFolder, 'build')),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '../example'),
+    compress: true,
+    hot: true,
+    overlay: true,
+    stats: 'errors-only',
+    watchContentBase: true,
+    port: 9000,
+    host: 'localhost',
+    setup: (app) => {
+      app.set('view engine', 'ejs');
+
+      app.get('/', (_, res) => {
+        console.log(res);
+        return res.render('index');
+      });
+    },
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(env),
+    }),
+  ],
+};

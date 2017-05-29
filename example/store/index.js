91f6fa62fc8df5110a3509cabbd9694abdc8e431
hajji tarek <hajji-tarik@outlook.fr>
2017-05-29 11:13:05 +0000

    redux store

--- example/store/index.js
+++ example/store/index.js
@@ -1,5 +1,29 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './reducers';
-import navigationMiddleware from '../../src/middleware';
 
-export default createStore(reducer, {}, applyMiddleware(navigationMiddleware));
+export default class ConfigureStore {
+  constructor(preloadedState = {}) {
+    const middlewares = [];
+    let composeEnhancers = compose;
+    if (process.env.NODE_ENV === 'development' && typeof window !== 'undefined') {
+      composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || composeEnhancers;
+    }
+    const enhancer = applyMiddleware(...middlewares);
+    const store = createStore(
+      reducer,
+      preloadedState,
+      composeEnhancers(enhancer),
+    );
+    this.hmrReducers();
+
+    Object.assign(this, store);
+  }
+
+  hmrReducers = () => {
+    if (module.hot && process.env.NODE_ENV === 'development') {
+      module.hot.accept('../Reducers', () => {
+        this.replaceReducer(require('./reducers')); // eslint-disable-line global-require
+      });
+    }
+  }
+}
